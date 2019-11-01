416c11fc890bdcf4d9e9b8c9ccbeca5119a987ad
Alumno <itonygame@gmail.com>
2019-11-01 13:55:09 -0700

    Creacion de app con eventos

---
+++ Eventos/ViewControllers/CeldaEventoController.swift
@@ -0,0 +1,17 @@
+//
+//  CeldaEventoController.swift
+//  Eventos
+//
+//  Created by Alumno on 11/1/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaEventoController : UITableViewCell {
+    
+    @IBOutlet weak var lblNombreEvento: UILabel!
+    @IBOutlet weak var lblFecha: UILabel!
+    @IBOutlet weak var imgFlyer: UIImageView!
+}
---
+++ Eventos/ViewControllers/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Eventos
+//
+//  Created by Alumno on 11/1/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    
+    var eventos : [Evento] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "cellEvento") as? CeldaEventoController
+        celda?.lblNombreEvento.text = eventos[indexPath.row].nombre
+        celda?.lblFecha.text = eventos[indexPath.row].fecha
+        return celda!
+    }
+
+}
+
